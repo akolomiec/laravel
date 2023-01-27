f150d1b780c9b33ca594102565dfe23b11c39113
Alexandr Kolomiec <akolomiec@mail.ru>
2023-01-27 16:15:49 +0500

    Cоздание Route с необязательными параметрами и проверкой по RegEx в Laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -32,3 +32,18 @@ Route::get('/post/{id}', function ($id) {
 Route::get('/post/{id}', function ($id) {
     return 'Пост id = ' . $id;
 });
+
+
+Route::get('/post/{post}/{tag}', function ($id, $tagid){
+    return "Пост id= " . $id . " tagid= " . $tagid;
+});
+
+Route::get('/postv2/{post?}', function ($id=0){
+    return "Пост id= " . $id;
+});
+
+Route::get('/users/{id?}/{name?}', function ($id = 0, $name = 'vasja'){
+    return "User " . $id . ". Name " . $name;
+})->where(['id' => '[0-9]+', 'name' => '[A-Za-z]+']);
+
+
